7af08190602355c592fc01d2b4b34bf26fdb71b9
Richard <rickanimals02@gmail.com>
2021-08-29 17:18:39 -0300

    Primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let config = document.querySelector(".config")
+let body = document.querySelector("body")
+
+config.addEventListener('click', ()=>{
+    body.style.backgroundColor = "#fff"
+    body.style.color = "#000"
+
+})
