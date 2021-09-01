250712a8e29471f1660640115e99e4f50212698c
Richard <rickanimals02@gmail.com>
2021-08-31 21:34:29 -0300

    Script do slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,36 @@ config.addEventListener('click', ()=>{
     body.style.color = "#000"
 
 })
+
+let numberOfIcons = document.querySelectorAll('.icon').length;
+
+let currentSlide = 0;
+
+function goPrev()
+{
+    currentSlide--;
+    if(currentSlide < 0)
+    {
+        currentSlide = numberOfIcons - 1;
+    }
+    updateMargin();
+}
+
+function goNext()
+{
+    currentSlide++;
+    if(currentSlide > (numberOfIcons - 1))
+    {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin()
+{
+    let newMargin = currentSlide * 260;
+
+    document.querySelector('.icons').style.marginLeft = `-${newMargin}px`;
+}
+
+//let timer = setInterval(goNext, 5000);
